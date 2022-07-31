61018465b3825e8422f4d8d2339eba73ecd8b1c8
jishnuedayillyam <jishnue@qburst.com>
2022-07-31 13:19:38 +0530

    initial setup

---
+++ index.js
@@ -0,0 +1,6 @@
+import React from 'react'
+import ReactDom from 'react-dom'
+import App from './src/App'
+
+console.log(document.getElementById("root"));
+ReactDom.render(<App />, document.getElementById("root"))
---
+++ src/App.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>Appsdsdsad sd</div>
+  )
+}
+
+export default App
